bd8a60f503d5d98dc00b96c4d443e01286b59665
Tur.4000 <tur.4000@gmail.com>
2016-09-16 22:43:46 +0300

    Дописал строку

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Привет МИР!");
+            Console.WriteLine("Я хочу изучать C#!");
             Console.ReadLine();
         }
     }
