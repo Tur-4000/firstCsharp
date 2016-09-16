b8b42f878a49aa43cf1e6adec07302833ce7def4
Tur.4000 <tur.4000@gmail.com>
2016-09-16 23:16:27 +0300

    Доработал запрос имени и приветствие

--- HelloWorld/CinemaAdviser/Program.cs
+++ HelloWorld/CinemaAdviser/Program.cs
@@ -10,11 +10,10 @@ namespace CinemaAdviser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Здравствуйте, дорогой друг. Эта программа поможет подобрать вам фильм для вечернего просмотра");
-
-            String name = "Валерий";
-            String hello = "Здравствуйте, " + name;
-            Console.WriteLine(hello);
+            Console.WriteLine("Эта программа поможет подобрать вам фильм для вечернего просмотра");
+            Console.WriteLine("Как вас зовут?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + name);
 
             Console.ReadLine();
         }
