6c1f0b74eb586d99a4f37df6c8f4eab202d61d46
Tur.4000 <tur.4000@gmail.com>
2016-09-18 19:34:34 +0300

    Дописал выбор комедий

--- HelloWorld/CinemaAdviser/Program.cs
+++ HelloWorld/CinemaAdviser/Program.cs
@@ -15,6 +15,18 @@ namespace CinemaAdviser
             String name = Console.ReadLine();
             Console.WriteLine("Здравствуйте, " + name);
 
+            Console.WriteLine("Фильм какого жанра вы бы хотели посмотреть: комедия или драма?");
+            String genre = Console.ReadLine();
+
+            if(genre == "комедия")
+            {
+                Console.WriteLine("В таком случае, рекомендую посмотреть вам следующие фильмы:");
+                Console.WriteLine("* Ивын Васильевич меняет профессию");
+                Console.WriteLine("* Операция «Ы» и другие приключения Шурика");
+                Console.WriteLine("* Назад в будущее");
+                Console.WriteLine("* В джазе только девушки");
+            }
+
             Console.ReadLine();
         }
     }
