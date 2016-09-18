332acb8db08c61c9a9c9add7aa328e59d90becb4
Tur.4000 <tur.4000@gmail.com>
2016-09-18 22:38:32 +0300

    Написл черепашкой слово ТОРТ

--- HelloWorld/TurtleTraining/Program.cs
+++ HelloWorld/TurtleTraining/Program.cs
@@ -9,30 +9,89 @@ namespace TurtleTraining
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WriteT(int a, int b)
         {
-            Turtle.Speed = 9;
-            int i = 0;
-            while (i < 6)
+            // Напишем букву Т
+            Turtle.Angle = 0;
+            Turtle.Move(a);
+            Turtle.TurnLeft();
+            Turtle.Move(b);
+            Turtle.Angle = 90;
+            Turtle.Move(b*2);
+        }
+
+        static void WriteO(int a, int b)
+        {
+            Turtle.Angle = 0;
+            for (int i = 0; i < 2; i++)
             {
-                Turtle.Move(100);
-                Turtle.Turn(60);
-                i++;
+                Turtle.Move(a);
+                Turtle.TurnRight();
+                Turtle.Move(b);
+                Turtle.TurnRight();
+            }
+        }
+
+        static void WriteR(int a, int b)
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(a);
+            for(int i=0; i < 3; i++)
+            {
+                Turtle.TurnRight();
+                Turtle.Move(b);
             }
 
-            i = 0;
-            while (i < 4)
-            {
-                Turtle.Move(20);
-                Turtle.TurnRight();
-                Turtle.Move(20);
-                Turtle.TurnRight();
-                Turtle.Move(20);
-                Turtle.TurnLeft();
-                Turtle.Move(20);
-                Turtle.TurnLeft();
-                i++;
-            }
+        }
+
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 8;
+            Turtle.X = 200;
+            Turtle.Y = 200;
+
+            WriteT(60, 20);
+
+            // Пишем букву О
+            Turtle.X = 240;
+            Turtle.Y = 200;
+
+            WriteO(60, 40);
+
+            // Пишем букву P
+            Turtle.X = 300;
+            Turtle.Y = 200;
+
+            WriteR(60, 40);
+
+            Turtle.X = 370;
+            Turtle.Y = 200;
+
+            WriteT(60, 20);
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Turtle.Move(100);
+            //    Turtle.Turn(60);
+            //}
+
+            //for (int i =0;  i < 8; i++)
+            //{
+            //    Turtle.Move(100);
+            //    Turtle.Turn(45);
+            //}
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Turtle.Move(20);
+            //    Turtle.TurnRight();
+            //    Turtle.Move(20);
+            //    Turtle.TurnRight();
+            //    Turtle.Move(20);
+            //    Turtle.TurnLeft();
+            //    Turtle.Move(20);
+            //    Turtle.TurnLeft();
+            //}
         }
     }
 }
