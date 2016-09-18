1af6def03fc0f1b6db84f2e5aa35e96bf4cb0aeb
Tur.4000 <tur.4000@gmail.com>
2016-09-18 20:27:42 +0300

    Практиковался с числами

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -12,6 +8,16 @@ namespace HelloWorld
         {
             Console.WriteLine("Привет МИР!");
             Console.WriteLine("Я хочу изучать C#!");
+
+            Console.WriteLine("Введите два числа");
+
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сумма = " + (a+b));
+            Console.WriteLine("Произведение = " + (a * b));
+            Console.WriteLine("Среднее арифметическое = " + ((double)(a + b)/2));
+
             Console.ReadLine();
         }
     }
