6a002070373c98c13bc61606be90bc3aab3db32e
Tur.4000 <tur.4000@gmail.com>
2016-09-18 23:40:45 +0300

    Научили черепашку двигаться по полю и реагировать на нажатия клавиш. Разместили еду на поле.

--- HelloWorld/TurtleGame/Program.cs
+++ HelloWorld/TurtleGame/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
 
 namespace TurtleGame
 {
@@ -10,6 +11,35 @@ namespace TurtleGame
     {
         static void Main(string[] args)
         {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            Turtle.PenUp();
+
+            GraphicsWindow.BrushColor = "Red";
+            var eat = Shapes.AddRectangle(10, 10);
+            Shapes.Move(eat, 200, 200);
+
+            while (true)
+            {
+                Turtle.Speed = 5;                
+                Turtle.Move(10);
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if(GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            } else if(GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            } else if(GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            } else if(GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
         }
     }
 }
