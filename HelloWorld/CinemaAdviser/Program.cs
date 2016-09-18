29b68564686dae8f9dfc141b4c852a36b16b67fa
Tur.4000 <tur.4000@gmail.com>
2016-09-18 20:42:29 +0300

    изменил цвет выводимого текста

--- HelloWorld/CinemaAdviser/Program.cs
+++ HelloWorld/CinemaAdviser/Program.cs
@@ -12,12 +12,13 @@ namespace CinemaAdviser {
             String name = Console.ReadLine();
             Console.WriteLine("Здравствуйте, " + name);
 
-            Console.WriteLine("Фильм какого жанра вы бы хотели посмотреть: комедия, драма, фантастика, эротика?");
+            Console.WriteLine("Фильм какого жанра вы бы хотели посмотреть: комедия, драма, фантастика, эротика, триллер, боевик?");
             String genre = Console.ReadLine();
 
             Console.WriteLine("В таком случае, рекомендую посмотреть вам следующие фильмы:");
 
             if (genre == "комедия") {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("* Иван Васильевич меняет профессию");
                 Console.WriteLine("* Операция «Ы» и другие приключения Шурика");
                 Console.WriteLine("* Назад в будущее");
@@ -29,10 +30,12 @@ namespace CinemaAdviser {
                 Console.WriteLine("* Догма");
                 Console.WriteLine("* В погоне за Эми");
             } else if (genre == "фантастика") {
+                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("* Люди икс");
                 Console.WriteLine("* Стражи галактики");
                 Console.WriteLine("* Люди в чёрном");
             } else if (genre == "эротика") {
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine("Скоько вам лет?");
                 int years = Int32.Parse(Console.ReadLine());
                 if(years < 18) {
@@ -42,7 +45,16 @@ namespace CinemaAdviser {
                     Console.WriteLine("* Эммануэль");
                     Console.WriteLine("* Греческая смоковница");
                 }
+            } else if(genre == "триллер") {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("* Девушка с татуировкой дракона");
+                Console.WriteLine("* Воздушный маршал");
+            } else if(genre == "боевик") {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("* Джейсон Борн");
+                Console.WriteLine("* Командо");
             } else {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("* Зелёная миля");
                 Console.WriteLine("* Побег из Шоушенка");
             }
