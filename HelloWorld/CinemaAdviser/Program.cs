4a2d537c4e9743118a11b83a03f0358313ce9aa3
Tur.4000 <tur.4000@gmail.com>
2016-09-18 19:46:06 +0300

    Добавил драмы

--- HelloWorld/CinemaAdviser/Program.cs
+++ HelloWorld/CinemaAdviser/Program.cs
@@ -18,13 +18,25 @@ namespace CinemaAdviser
             Console.WriteLine("Фильм какого жанра вы бы хотели посмотреть: комедия или драма?");
             String genre = Console.ReadLine();
 
-            if(genre == "комедия")
-            {
-                Console.WriteLine("В таком случае, рекомендую посмотреть вам следующие фильмы:");
-                Console.WriteLine("* Ивын Васильевич меняет профессию");
+            Console.WriteLine("В таком случае, рекомендую посмотреть вам следующие фильмы:");
+
+            if (genre == "комедия")
+            {                
+                Console.WriteLine("* Иван Васильевич меняет профессию");
                 Console.WriteLine("* Операция «Ы» и другие приключения Шурика");
                 Console.WriteLine("* Назад в будущее");
                 Console.WriteLine("* В джазе только девушки");
+                Console.WriteLine("* Клерки");
+                Console.WriteLine("* Клерки 2");
+                Console.WriteLine("* Тусовщики из супермаркета");
+                Console.WriteLine("* Джей и Молчаливый Боб наносят ответный удар");
+                Console.WriteLine("* Догма");
+                Console.WriteLine("* В погоне за Эми");
+            }
+            else
+            {
+                Console.WriteLine("* Зелёная миля");
+                Console.WriteLine("* Побег из Шоушенка");
             }
 
             Console.ReadLine();
